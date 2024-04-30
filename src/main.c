f26312ed97f5fbf39415580c90d35dcc6df5db9b
galliume <gcconantc@gmail.com>
2024-05-01 00:44:34 +0200

    doom fire!

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 #include <SDL2/SDL.h>
 
@@ -15,60 +18,48 @@ bool is_running = false;
 int previous_frame_time = 0;
 float fov_factor = 640;
 
-triangle_t *triangles_to_render = NULL;
 vec3_t camera_position = { .x = 0, .y = 0, .z = 0 };
+
+unsigned char fireRGB[111] = {
+  0x07,0x07,0x07,0x1F,0x07,0x07,0x2F,0x0F,0x07,0x47,0x0F,0x07,0x57,0x17,0x07,0x67,
+  0x1F,0x07,0x77,0x1F,0x07,0x8F,0x27,0x07,0x9F,0x2F,0x07,0xAF,0x3F,0x07,0xBF,0x47,
+  0x07,0xC7,0x47,0x07,0xDF,0x4F,0x07,0xDF,0x57,0x07,0xDF,0x57,0x07,0xD7,0x5F,0x07,
+  0xD7,0x5F,0x07,0xD7,0x67,0x0F,0xCF,0x6F,0x0F,0xCF,0x77,0x0F,0xCF,0x7F,0x0F,0xCF,
+  0x87,0x17,0xC7,0x87,0x17,0xC7,0x8F,0x17,0xC7,0x97,0x1F,0xBF,0x9F,0x1F,0xBF,0x9F,
+  0x1F,0xBF,0xA7,0x27,0xBF,0xA7,0x27,0xBF,0xAF,0x2F,0xB7,0xAF,0x2F,0xB7,0xB7,0x2F,
+  0xB7,0xB7,0x37,0xCF,0xCF,0x6F,0xDF,0xDF,0x9F,0xEF,0xEF,0xC7,0xFF,0xFF,0xFF
+};
+
+#define length ((sizeof(fireRGB) / sizeof(fireRGB[0])) / 3)
+
+vec3_t palette[length];
+
+uint32_t* pixel_buffer = NULL;
 
 void render(void)
 {
   //SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
   //SDL_RenderClear(renderer);
 
-  draw_grid();
-
-  int num_triangles = array_length(triangles_to_render);
-
-  for (int i = 0; i < num_triangles; ++i)
+  for (int x = 0; x < window_width; ++x)
   {
-    triangle_t triangle = triangles_to_render[i];
-
-    if (render_method == RENDER_FILL_TRIANGLE || render_method == RENDER_FILL_TRIANGLE_WIRE)
+    for (int y = 0; y < window_height; ++y)
     {
-      draw_filled_triangle(
-        triangle.points[0].x, triangle.points[0].y,
-        triangle.points[1].x, triangle.points[1].y,
-        triangle.points[2].x, triangle.points[2].y,
-        triangle.color);
-    }
+      uint32_t index = pixel_buffer[window_width * y + x];
+      vec3_t color = palette[index];
 
-    if (render_method == RENDER_WIRE || render_method == RENDER_FILL_TRIANGLE_WIRE || render_method == RENDER_WIRE_VERTEX)
-    {
-      draw_triangle(
-        triangle.points[0].x, triangle.points[0].y,
-        triangle.points[1].x, triangle.points[1].y,
-        triangle.points[2].x, triangle.points[2].y,
-        0xFF00FF00);
-    }
-
-    if (render_method == RENDER_WIRE_VERTEX)
-    {
-      draw_rect(triangle.points[0].x - 3, triangle.points[0].y - 3, 6, 6, 0xFF0000);
-      draw_rect(triangle.points[1].x - 3, triangle.points[1].y - 3, 6, 6, 0xFF0000);
-      draw_rect(triangle.points[2].x - 3, triangle.points[2].y - 3, 6, 6, 0xFF0000);
+      color_buffer[(window_width * y + x)] = (uint8_t)255 << 24 | (uint8_t)color.x << 16 | (uint8_t)color.y << 8 | (uint8_t)color.z;
     }
   }
-  array_free(triangles_to_render);
 
   render_color_buffer();
-  clear_color_buffer(0xFF000000);
+  clear_color_buffer(0xFF00FF00);
 
   SDL_RenderPresent(renderer);
 }
 
 void setup(char *obj_file)
 {
-  render_method = RENDER_WIRE;
-  cull_method = CULL_BACKFACE;
-
   color_buffer = malloc(sizeof(uint32_t) * window_width * window_height);
 
   if (!color_buffer)
@@ -90,8 +81,39 @@ void setup(char *obj_file)
     is_running = false;
   }
 
-  load_cube_mesh_data();
-  //load_obj_file_data(obj_file);
+  pixel_buffer = malloc(sizeof(uint32_t) * window_width * window_height);
+
+  if (!pixel_buffer)
+  {
+    fprintf(stderr, "Can't malloc pixel_buffer");
+    is_running = false;
+  }
+
+  for (int y = 0; y < 37; ++y)//36 colors in palette
+  {
+    //xyz = rgb
+    vec3_t color = {
+      .x = fireRGB[y * 3 + 0],
+      .y = fireRGB[y * 3 + 1],
+      .z = fireRGB[y * 3 + 2]
+    };
+
+    palette[y] = color;
+  }
+
+  for (int i = 0; i < window_width * window_height; i++)
+  {
+    pixel_buffer[i] = 0;//black
+  }
+  for (int i = 0; i < window_width; i++)
+  {
+    for (int y = 0; y < 10; y++)
+    {
+      pixel_buffer[(window_height - 1) * window_width + i] = 36;//36 = white
+    }
+  }
+
+  srand(time(NULL));
 }
 
 void process_input(void)
@@ -109,42 +131,23 @@ void process_input(void)
       {
         is_running = false;
       }
-      if (event.key.keysym.sym == SDLK_1)
-      {
-        render_method = RENDER_WIRE;
-      }
-      if (event.key.keysym.sym == SDLK_2)
-      {
-        render_method = RENDER_WIRE_VERTEX;
-      }
-      if (event.key.keysym.sym == SDLK_3)
-      {
-        render_method = RENDER_FILL_TRIANGLE;
-      }
-      if (event.key.keysym.sym == SDLK_4)
-      {
-        render_method = RENDER_FILL_TRIANGLE_WIRE;
-      }
-      if (event.key.keysym.sym == SDLK_c)
-      {
-        cull_method = CULL_BACKFACE;
-      }
-      if (event.key.keysym.sym == SDLK_d)
-      {
-        cull_method = CULL_NONE;
-      }
-      break;
   }
 }
 
-vec2_t project(vec3_t point)
+void fire(uint32_t from)
 {
-  vec2_t projected_point = {
-    .x = (fov_factor * point.x) / point.z,
-    .y = (fov_factor * point.y) / point.z
-  };
+  uint32_t pixel = pixel_buffer[from];
 
-  return projected_point;
+  if (pixel == 0)
+  {
+    pixel_buffer[from - window_width] = pixel_buffer[from];
+  }
+  else
+  {
+    uint32_t randIx = round((rand() % RAND_MAX) / (double)RAND_MAX * 3.0);
+    uint32_t to = from - randIx + 1;
+    pixel_buffer[to - window_width] = pixel - (randIx & 1);
+  }
 }
 
 void update(void)
@@ -158,105 +161,12 @@ void update(void)
 
   previous_frame_time = SDL_GetTicks();
 
-  triangles_to_render = NULL;
-
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
-  mesh.rotation.z += 0.01;
-
-  mesh.scale.x += 0.002;
-
-  mat4_t scale_m = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
-
-  int num_faces = array_length(mesh.faces);
-
-  for (int i = 0; i < num_faces; ++i)
+  for (int x = 0; x < window_width; x++)
   {
-    face_t mesh_face = mesh.faces[i];
-
-    vec3_t face_vertices[3];
-    face_vertices[0] = mesh.vertices[mesh_face.a - 1];
-    face_vertices[1] = mesh.vertices[mesh_face.b - 1];
-    face_vertices[2] = mesh.vertices[mesh_face.c - 1];
-
-    vec4_t transformed_vertices[3];
-
-    for (int j = 0; j < 3; ++j)
+    for (int y = 1; y < window_height; y++)
     {
-      vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
-      transformed_vertex = mat4_mul_vec4(scale_m, transformed_vertex);
-
-      // transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
-      // transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
-      // transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
-
-      transformed_vertex.z += 5;
-
-      transformed_vertices[j] = transformed_vertex;
+      fire(y * window_width + x);
     }
-
-    if (cull_method == CULL_BACKFACE)
-    {
-      vec3_t vec_a = vec3_from_vec4(transformed_vertices[0]);
-      vec3_t vec_b = vec3_from_vec4(transformed_vertices[1]);
-      vec3_t vec_c = vec3_from_vec4(transformed_vertices[2]);
-
-      vec3_t vector_ab = vec3_sub(vec_b, vec_a);
-      vec3_t vector_ac = vec3_sub(vec_c, vec_a);
-
-      vec3_normalize(&vector_ab);
-      vec3_normalize(&vector_ac);
-
-      vec3_t normal = vec3_cross(vector_ab, vector_ac);//left handed coord system
-      vec3_normalize(&normal);
-
-      vec3_t camera_ray = vec3_sub(camera_position, vec_a);
-
-      float dot_normal_camera = vec3_dot(normal, camera_ray);
-
-      if (dot_normal_camera < 0) continue; //simple back face culling
-    }
-
-    vec2_t projected_points[3];
-
-    for (int j = 0; j < 3; ++j)
-    {
-      projected_points[j] = project(vec3_from_vec4(transformed_vertices[j]));
-      projected_points[j].x += (window_width / 2);
-      projected_points[j].y += (window_height / 2);
-
-    }
-
-    float average_depth =
-      (transformed_vertices[0].z + transformed_vertices[1].z + transformed_vertices[2].z) / 3.0;
-
-    triangle_t projected_triangle = {
-      .points = {
-        { projected_points[0].x, projected_points[0].y },
-        { projected_points[1].x, projected_points[1].y },
-        { projected_points[2].x, projected_points[2].y }
-      },
-      .color = mesh_face.color,
-      .average_depth = average_depth
-    };
-
-    //simple bubble sort.
-    //@todo change if too slow
-    int num_triangles = array_length(triangles_to_render);
-
-    for (int i = 0; i < num_triangles; ++i)
-    {
-      for (int j = i; j < num_triangles; ++j)
-      {
-        if (triangles_to_render[i].average_depth < triangles_to_render[j].average_depth)
-        {
-          triangle_t temp = triangles_to_render[i];
-          triangles_to_render[i] = triangles_to_render[j];
-          triangles_to_render[j] = temp;
-        }
-      }
-    }
-    array_push(triangles_to_render, projected_triangle);
   }
 }
 
@@ -265,6 +175,7 @@ void free_resources(void)
   array_free(mesh.faces);
   array_free(mesh.vertices);
 
+  free(pixel_buffer);
   free(color_buffer);
 }
 
